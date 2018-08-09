f6cbdf57a9b56e7e8789fc34155ca2e6ec0c9b74
Leon Maxwell <wxmaxx@gmail.com>
2018-08-08 19:47:43 -0500

    added code attribution to file

--- week-2/hello-world/maxwell-exercise-2.2.js
+++ week-2/hello-world/maxwell-exercise-2.2.js
@@ -1,3 +1,22 @@
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   4 December 2017
+; Modified By: <Leon Maxwell>
+; Modified date: 08 August 2018
+; Description: Hello World with express
+;===========================================
+*/ 
+
+// Calls in the header.js file located in the root directory
+
+var header = require('../../header.js');
+
+// Adding header information
+console.log(header.display("Leon", "Maxwell", "Exercise 2.2")); 
+console.log("\n");
+
 var express = require('express');
 var http = require('http');
 
