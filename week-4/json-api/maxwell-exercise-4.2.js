cfc52b973c85a8393e2982a8c42fa32ca9a1283b
Leon Maxwell <wxmaxx@gmail.com>
2018-08-22 08:36:13 -0500

    initial week 4 files

---
+++ week-4/json-api/maxwell-exercise-4.2.js
@@ -0,0 +1,41 @@
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   9 December 2017
+; Modified By: <Leon Maxwell>
+; Modified date: 22 August 2018
+; Description: Advanced Routing
+;===========================================
+*/ 
+
+// Calls in the header.js file located in the root directory
+
+var header = require('../../header.js');
+
+//set requires
+var express = require("express");
+var http = require("http");
+
+
+//start express
+var app = express();
+
+
+//make a request to retrieve customer ID
+app.get("/customer/:id", function (req, res) {
+	var id = parseInt(request.params.id, 10);
+
+  response.json({
+        firstName: "Leon",
+        lastName: "Maxwell",
+    	//links to the var id
+        employeeId: id
+    });
+});
+
+//start the server and log a response to ensure it is running
+http.createServer(app).listen(8080, function() {
+    console.log("Application started on port 8080");
+});
+
