232a424a5dd326c4045a6d0d04e3b514ef9e07c7
Leon Maxwell <wxmaxx@gmail.com>
2018-08-22 10:08:36 -0500

    delete crap

--- week-4/json-api/maxwell-exercise-4.2.js
+++ week-4/json-api/maxwell-exercise-4.2.js
@@ -20,26 +20,30 @@ console.log("\n");
 //set requires
 var express = require("express");
 var http = require("http");
+var logger = require('morgan');
 
 
 //start express
 var app = express();
 
+app.use(logger('dev'));
+
 
 //make a request to retrieve customer ID
 app.get("/customer/:id", function (req, res) {
-	var id = parseInt(request.params.id, 5);
+	var id = parseInt(request.params.id, 10);
 
   response.json({
         firstName: "Leon",
         lastName: "Maxwell",
+        middleName: "E",
     	//links to the var id
         employeeId: id
     });
 });
 
 //start the server and log a response to ensure it is running
-http.createServer(app).listen(8080, function() {
-    console.log("Application started on port 8080");
+http.createServer(app).listen(3000, function() {
+    console.log("Application started on port 3000");
 });
 
