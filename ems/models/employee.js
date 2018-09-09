4f2efbe4d1db15e83966d9ee8ecd9c9eb16b14e2
Leon Maxwell <wxmaxx@gmail.com>
2018-09-09 05:38:31 -0500

    Updated ems files

---
+++ ems/models/employee.js
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title:  Program Header
+; Author: Professor Krasso 
+; Date:   11 December 2017
+; Modified By: Leon Maxwell
+; Modified date: 09 September 2018
+; Description: EMS employee model page
+;===========================================
+*/
+
+// required
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+
+// define the employeeSchema
+var employeeSchema = new Schema({
+    name: String
+});
+
+
+// define the employee model
+var Employee = mongoose.model("Employee", employeeSchema);
+
+
+// expose the fruit to calling files
+module.exports = Employee;
