e7d87f2ffde485c41666dbe93568bc3c1f321eff
Leon Maxwell <wxmaxx@gmail.com>
2018-09-28 21:27:22 -0500

    Closer!

--- ems/app.js
+++ ems/app.js
@@ -11,28 +11,31 @@
 
 //set includes
 // require statements
-var express = require("express");
-var http = require("http");
-var path = require("path");
-var logger = require("morgan");
-var helmet = require("helmet");
-var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
-var csrf = require("csurf");
-var mongoose = require("mongoose");
-var Employee = require("./models/employee");
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+var helmet = require('helmet');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var csrf = require('csurf');
+var mongoose = require('mongoose');
+var Employee = require('./models/employee');
 
 // database
-var mongoDB = "mongodb://admin:admin1@ds141932.mlab.com:41932/ems";
-mongoose.connect(mongoDB, {
+var mongoDB = 'mongodb://admin:admin1@ds141932.mlab.com:41932/ems';
+mongoose.connect(
+  mongoDB,
+  {
     useMongoClient: true
-  });
+  }
+);
 
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error: "));
-db.once("open", function() {
-  console.log("Application connected to mLab MongoDB instance");
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+db.once('open', function() {
+  console.log('Application connected to mLab MongoDB instance');
 });
 
 // setup csrf protection
@@ -42,10 +45,12 @@ var csrfProtection = csrf({ cookie: true });
 var app = express();
 
 // use statements
-app.use(logger("short"));
-app.use(bodyParser.urlencoded({
+app.use(logger('short'));
+app.use(
+  bodyParser.urlencoded({
     extended: true
-  }));
+  })
+);
 
 app.use(cookieParser());
 app.use(helmet.xssFilter());
@@ -58,31 +63,31 @@ app.use(function(request, response, next) {
 });
 
 // set statements
-app.set("views", path.resolve(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8080);
 
 // route requests
-app.get("/", function(request, response) {
-  response.render("index", {
-    title: "Maxwell Home Page"
+app.get('/', function(request, response) {
+  response.render('index', {
+    title: 'Maxwell Home Page'
   });
 });
 
-app.get("/new", function(request, response) {
-  response.render("new", {
-    title: "New Employee"
+app.get('/new', function(request, response) {
+  response.render('new', {
+    title: 'New Employee'
   });
 });
 
-app.post("/process", function(request, response) {
+app.post('/process', function(request, response) {
   // console.log(request.body.txtName);
   if (!request.body.txtName) {
-    response.status(400).send("Entries must have a name");
+    response.status(400).send('Entries must have a name');
     return;
   }
 
-  // get the request's form data
+  // get the request"s form data
   var employeeName = request.body.txtName;
   console.log(employeeName);
 
@@ -94,41 +99,41 @@ app.post("/process", function(request, response) {
   // save
   employee.save(function(error) {
     if (error) throw error;
-    console.log(employeeName + " saved successfully!");
+    console.log(employeeName + ' saved successfully!');
   });
-  response.redirect("/list");
+  response.redirect('/list');
 });
 
-app.get("/list", function(request, response) {
+app.get('/list', function(request, response) {
   Employee.find({}, function(error, employees) {
     if (error) throw error;
 
-    response.render("list", {
-      title: "Employee List",
+    response.render('list', {
+      title: 'Employee List',
       employees: employees
     });
   });
 });
 
-app.get("/view/:queryName", function(request, response) {
+app.get('/view/:queryName', function(request, response) {
   var queryName = request.params.queryName;
 
-  Employee.find({ 'name': queryName }, function(error, employees) {
+  Employee.find({ name: queryName }, function(error, employees) {
     if (error) throw error;
 
     console.log(employees);
 
     if (employees.length > 0) {
-      response.render("view", {
-        title: "Employee Record",
+      response.render('view', {
+        title: 'Employee Record',
         employee: employees
       });
     } else {
-      response.redirect("/list");
+      response.redirect('/list');
     }
   });
 });
 
-http.createServer(app).listen(app.get("port"), function() {
-  console.log("Application started on port " + app.get("port"));
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Application started on port ' + app.get('port'));
 });
